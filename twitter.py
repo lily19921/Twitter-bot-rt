911bc7b2ec04066d7daebfd40b01b4c2b219bb81
lily19921 <87338364+lily19921@users.noreply.github.com>
2021-07-14 22:35:11 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -6,8 +6,8 @@ CONSUMER_KEY =environ['CONSUMER_KEY']
 CONSUMER_SECRET =environ['CONSUMER_SECRET']
 ACCESS_KEY=environ ['ACCESS_KEY']
 ACCESS_SECRET=environ ['ACCESS_SECRET']
-auth = tweepy.OAuthHandler('CONSUMER_KEY ','CONSUMER_SECRET')
-auth.set_access_token('ACCESS_KEY','ACCESS_SECRET')
+auth = tweepy.OAuthHandler(CONSUMER_KEY , CONSUMER_SECRET')
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET')
 api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user=api.me()
 search='twitch.tv'
