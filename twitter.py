814e66656c2f0d3ca212250c6bd2ae080bcfb6ab
lily19921 <87338364+lily19921@users.noreply.github.com>
2021-07-14 22:41:08 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,10 +2,10 @@ import tweepy
 import time 
 import os
 from os import environ
-CONSUMER_KEY =environ['CONSUMER_KEY']
-CONSUMER_SECRET =environ['CONSUMER_SECRET']
-ACCESS_KEY=environ ['ACCESS_KEY']
-ACCESS_SECRET=environ ['ACCESS_SECRET']
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY= environ['ACCESS_KEY']
+ACCESS_SECRET= environ['ACCESS_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY , CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
