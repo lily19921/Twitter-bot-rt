6aec1c754c5d2d81e68bb7cb40b61e24535921aa
lily19921 <87338364+lily19921@users.noreply.github.com>
2021-07-28 16:04:13 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -15,7 +15,7 @@ def create_api():
     CONSUMER_SECRET = environ['CONSUMER_SECRET']
     ACCESS_KEY = environ['ACCESS_KEY']
     ACCESS_SECRET = environ['ACCESS_SECRET']
-    auth = tweepy.OAuthHandler('CONSUMER_KEY ','CONSUMER_SECRET')
+    auth = tweepy.OAuthHandler('CONSUMER_KEY','CONSUMER_SECRET')
     auth.set_access_token('ACCESS_KEY','ACCESS_SECRET')
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     try:
