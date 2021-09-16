e65e20be03f06ad3642cc8264cf7374a202f9602
lily19921 <87338364+lily19921@users.noreply.github.com>
2021-09-16 14:29:20 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,73 +1,24 @@
 import tweepy
-import logging
-import os
-import time
-logger = logging.getLogger()
+import time 
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+auth = tweepy.OAuthHandler('CONSUMER_KEY ','CONSUMER_SECRET')
+auth.set_access_token('ACCESS_KEY','ACCESS_SECRET')
+api=tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+user=api.me()
+search='twitch.tv'
+nrTweets=500
 
-def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logger.error("Error creating API", exc_info=True)
-        raise e
-    logger.info("API created")
-    return api
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
-class FavRetweetListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-
-    def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-        if not tweet.favorited:
-            # Mark it as Liked, since we have not done it yet
-            try:
-                tweet.favorite()
-                
-            except Exception as e:
-                logger.error("Error on fav", exc_info=True)
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error("Error on fav and retweet", exc_info=True)
-
-    def on_error(self, status):
-        logger.error(status)
-        
-def follow_followers(api):
-    logger.info("Retrieving and following followers")
-    for follower in tweepy.Cursor(api.followers).items():
-        if not follower.following:
-            logger.info(f"Following {follower.name}")
-            follower.follow()
-def main(keywords):
-    api = create_api()
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    time.sleep(60)
-    stream.filter(track=keywords, languages=["en"])
-    while True:
-        follow_followers(api)
-        logger.info("Waiting...")
+for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+    try :
+        print('Tweet Liked')
+        tweet.retweet()
+        tweet.favorite()
         time.sleep(60)
-
-if __name__ == "__main__":
-    main(["Python", "Tweepy"])
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
